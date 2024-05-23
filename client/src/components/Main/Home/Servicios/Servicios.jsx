d7c1ef28fd2feb5384977724512d393cb3cc6d88
2Tucho <albergromero@gmail.com>
2024-05-23 18:04:20 +0200

    Crear el elemento Servicios

--- client/src/components/Main/Home/Servicios/Servicios.jsx
+++ client/src/components/Main/Home/Servicios/Servicios.jsx
@@ -1,34 +1,60 @@
 import React from "react";
 
 const Servicios = () => {
-  return <article className="servicios">
-    <div>
-      <input type="checkbox" name="Orientación vocacional" />
-      <label htmlFor="">Orientación vocacional</label>
-      <input type="checkbox" />
-      <label htmlFor="">Año y trimestre escolar en el extranjero</label>
-      <input type="checkbox" />
-      <label htmlFor="">Selección de colegios en España</label>
-      <input type="checkbox" />
-      <label htmlFor="">Selección de colegios en España</label>
-      <input type="checkbox" />
-      <label htmlFor="">Estudiar en España: Grados y Postgrados</label>
-      <input type="checkbox" />
-    </div>
-    <div>
-      <label htmlFor="">Estudiar en el extranjero: Grados y Postgrados</label>
-      <input type="checkbox" />
-      <label htmlFor="">Preparación de exámenes</label>
-      <input type="checkbox" />
-      <label htmlFor="">Campamentos</label>
-      <input type="checkbox" />
-      <label htmlFor="">Cursos preuniversitarios</label>
-      <input type="checkbox" />
-      <label htmlFor="">Voluntariados</label>
-      <input type="checkbox" />
-      <label htmlFor="">Desarrollo personal</label>
-    </div>
-  </article>;
+  return <section className="servicios">
+    <h1>Servicios</h1>
+    <article>
+      <div>
+        <div>
+          <input type="checkbox" name="Orientación vocacional" />
+          <label htmlFor="">Orientación vocacional</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Año y trimestre escolar en el extranjero</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Selección de colegios en España</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Selección de colegios en España</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Estudiar en España: Grados y Postgrados</label>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Estudiar en el extranjero: Grados y Postgrados</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Preparación de exámenes</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Campamentos</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Cursos preuniversitarios</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Voluntariados</label>
+        </div>
+        <div>
+          <input type="checkbox" />
+          <label htmlFor="">Desarrollo personal</label>
+        </div>
+      </div>
+    </article>
+  </section>;
 };
 
 export default Servicios;
