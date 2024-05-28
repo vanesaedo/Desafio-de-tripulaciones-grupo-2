c2ae3be8b97428b305ead7684a1c803d62d1ef70
2Tucho <albergromero@gmail.com>
2024-05-28 14:26:58 +0200

    Se dibujan los datos en Servicios

--- client/src/components/Main/Home/Servicios/Servicios.jsx
+++ client/src/components/Main/Home/Servicios/Servicios.jsx
@@ -4,8 +4,8 @@ const Servicios = ({ clienteBuscado }) => {
   return <section className="servicios">
     <h1>Servicios</h1>
     <article>
-    {clienteBuscado? clienteBuscado.map(elem => <div>
-      {elem.interes_alumno_bool == true? <input type="checkbox" name={elem.interes_alumno} checked/> : <input type="checkbox" name={elem.interes_alumno} />}
+    {clienteBuscado? clienteBuscado.map((elem, i) => <div key={i}>
+      {elem.interes_alumno_bool == true? <input type="checkbox" name={elem.interes_alumno} defaultChecked/> : <input type="checkbox" name={elem.interes_alumno} />}
       <label htmlFor={elem.interes_alumno}>{elem.interes_alumno}</label>
     </div>) : <></>}
     </article>
