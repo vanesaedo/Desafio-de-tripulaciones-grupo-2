b9bb49af4a991833fb6577c9466581746da63f96
Vanesa Edo <vanesa.en@gmail.com>
2024-05-28 09:20:14 +0200

    Modificaciones sin éxito. Revisando pestañas a convertir en grupo de Tabs

--- client/src/components/Main/Home/Pestañas/Pestañas.jsx
+++ client/src/components/Main/Home/Pestañas/Pestañas.jsx
@@ -1,23 +1,43 @@
 import React from "react";
+import { Button, ButtonGroup } from "@mui/material";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
-const Pestañas = ({ setPestañaEnUso }) => {
-  function clickPestaña(pestaña) {
-    setPestañaEnUso(pestaña);
+
+
+
+
+const Pestanas = ({ setPestanaEnUso }) => {
+  const [ pestanaEnUso, setPestanaEnUso ] = useState(value);
+
+  function clickPestana(value) {
+    setPestanaEnUso(value);
+
   };
 
   return <>
-  <section className="cabecera_estudiante">
-    <div className="nombre_estudiante">
-      <h3>Nombre del alumno</h3>
-    </div>
-    <nav className="pestañas">
-      <button onClick={() => clickPestaña("datosPersonales")} className="boton-datos-personales">DATOS PERSONALES</button>
-      <button onClick={() => clickPestaña("servicios")} className="boton-servicios">SERVICIOS</button>
-      <button onClick={() => clickPestaña("interacciones")} className="boton-interacciones">INTERACCIONES</button>
-      <button onClick={() => clickPestaña("campañasContratadas")} className="boton-campañas-contratadas">CAMPAÑAS CONTRATADAS</button>
-    </nav>
+    <section className="cabecera_estudiante">
+      <div className="nombre_estudiante">
+        <h3>Nombre del alumno</h3>
+      </div>
+      <nav className="pestañas">
+
+        <Tabs value={pestana} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="secondary tabs example">
+          <Tab onChange={() => clickPestaña("datosPersonales")}value="datospersonales" label="Datos Personales" />
+          <Tab onChange={() => clickPestaña("interacciones")} className="boton-interacciones" value="interacciones" label="Interacciones" />
+          <Tab onChange={() => clickPestaña("campañasContratadas")} className="boton-campañas-contratadas" value="contratos" label="Contratos" />
+        </Tabs>
+
+        <ButtonGroup variant="text" aria-label="Basic button group">
+          <Button onClick={() => clickPestaña("datosPersonales")} className="boton-datos-personales">Datos personales</Button>
+          <Button onClick={() => clickPestaña("servicios")} className="boton-servicios">Preferencias</Button>
+          <Button onClick={() => clickPestaña("interacciones")} className="boton-interacciones">Interacciones</Button>
+          <Button onClick={() => clickPestaña("campañasContratadas")} className="boton-campañas-contratadas">Contratos</Button>
+        </ButtonGroup>
+
+      </nav>
     </section>
-  </> 
+  </>
 };
 
 export default Pestañas;
