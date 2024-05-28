e24f8098f6ee62fb2b94a637973fdf83054f6b57
Vanesa Edo <vanesa.en@gmail.com>
2024-05-28 09:28:26 +0200

    Corregidos errores en Pestañas.

--- client/src/components/Main/Home/Pestañas/Pestañas.jsx
+++ client/src/components/Main/Home/Pestañas/Pestañas.jsx
@@ -4,40 +4,33 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
 
-
-
-
-const Pestanas = ({ setPestanaEnUso }) => {
-  const [ pestanaEnUso, setPestanaEnUso ] = useState(value);
-
-  function clickPestana(value) {
-    setPestanaEnUso(value);
-
+const Pestañas = ({ setPestañaEnUso }) => {
+  function clickPestaña(pestaña) {
+    setPestañaEnUso(pestaña);
   };
 
   return <>
-    <section className="cabecera_estudiante">
-      <div className="nombre_estudiante">
-        <h3>Nombre del alumno</h3>
-      </div>
-      <nav className="pestañas">
+  <section className="cabecera_estudiante">
+    <div className="nombre_estudiante">
+      <h3>Nombre del alumno</h3>
+    </div>
 
-        <Tabs value={pestana} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="secondary tabs example">
+    <Tabs value={pestaña} onChange={handleChange} textColor="secondary" indicatorColor="secondary" aria-label="secondary tabs example">
           <Tab onChange={() => clickPestaña("datosPersonales")}value="datospersonales" label="Datos Personales" />
           <Tab onChange={() => clickPestaña("interacciones")} className="boton-interacciones" value="interacciones" label="Interacciones" />
           <Tab onChange={() => clickPestaña("campañasContratadas")} className="boton-campañas-contratadas" value="contratos" label="Contratos" />
         </Tabs>
 
-        <ButtonGroup variant="text" aria-label="Basic button group">
-          <Button onClick={() => clickPestaña("datosPersonales")} className="boton-datos-personales">Datos personales</Button>
-          <Button onClick={() => clickPestaña("servicios")} className="boton-servicios">Preferencias</Button>
-          <Button onClick={() => clickPestaña("interacciones")} className="boton-interacciones">Interacciones</Button>
-          <Button onClick={() => clickPestaña("campañasContratadas")} className="boton-campañas-contratadas">Contratos</Button>
-        </ButtonGroup>
-
-      </nav>
+    <nav className="pestañas">
+      <button onClick={() => clickPestaña("datosPersonales")} className="boton-datos-personales">DATOS PERSONALES</button>
+      <button onClick={() => clickPestaña("servicios")} className="boton-servicios">SERVICIOS</button>
+      <button onClick={() => clickPestaña("interacciones")} className="boton-interacciones">INTERACCIONES</button>
+      <button onClick={() => clickPestaña("campañasContratadas")} className="boton-campañas-contratadas">CAMPAÑAS CONTRATADAS</button>
+    </nav>
     </section>
-  </>
+  </> 
 };
+
+
 
 export default Pestañas;
